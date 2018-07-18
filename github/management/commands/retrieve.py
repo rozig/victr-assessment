5923e0f554dbfbc256390373a69af03abfb3abe0
Ganzorig Erdenebat <the.ganzorig25@gmail.com>
2018-07-17 22:21:14 -0500

    Modified retrieve command

--- github/management/commands/retrieve.py
+++ github/management/commands/retrieve.py
@@ -6,30 +6,36 @@ import requests, json
 class Command(BaseCommand):
     help = 'Retrieves most starred python project repos from github'
 
-    def add_arguments(self, parser):
-        parser.add_argument('url', nargs='+', type=str)
+    project_list = []
+    api_url = 'https://api.github.com/search/repositories?q=language:python&sort=stars&order=desc&per_page=100'
 
     def handle(self, *args, **options):
-        url = options['url'][0]
         self.stdout.write(self.style.WARNING('Retrieving data ...'))
-        projects = json.loads(requests.get(url).text)['items']
+        self.retrieve_data()
         self.stdout.write(self.style.WARNING('Retrieved!'))
-        projects_to_add = []
-        for proj in projects:
-            project = Project(
-                repository_id=proj['id'],
-                name=proj['name'],
-                description=proj['description'],
-                stars=proj['stargazers_count'],
-                url=proj['html_url'],
-                created_date=proj['created_at'],
-                last_pushed_date=proj['updated_at']
-            )
-            projects_to_add.append(project)
-
         self.stdout.write(self.style.WARNING('Removing old data ...'))
         Project.objects.all().delete()
         self.stdout.write(self.style.WARNING('Removed!'))
         self.stdout.write(self.style.WARNING('Inserting new data ...'))
-        Project.objects.bulk_create(projects_to_add)
+        Project.objects.bulk_create(self.project_list)
         self.stdout.write(self.style.SUCCESS('Finished!'))
+
+    def retrieve_data(self):
+        for page in range(1, 11):
+            url = self.api_url + '&page=' + str(page)
+            response = json.loads(requests.get(url).text)
+            if response.get('items') is None:
+                raise CommandError(response.get('message'))
+
+            projects = response['items']
+            for proj in projects:
+                project = Project(
+                    repository_id=proj['id'],
+                    name=proj['name'],
+                    description=proj['description'],
+                    stars=proj['stargazers_count'],
+                    url=proj['html_url'],
+                    created_date=proj['created_at'],
+                    last_pushed_date=proj['pushed_at']
+                )
+                self.project_list.append(project)
